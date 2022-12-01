ce9fbfc115b3320ed0d3dc0781b552959e368b7e
Lerr1394 <87046180+Lerr1394@users.noreply.github.com>
2022-12-01 18:57:20 -0300

    atualização com graficos e header responsivo

--- script/graficos.js
+++ script/graficos.js
@@ -1,5 +1,10 @@
 google.charts.load('current', {'packages':['corechart']});
 
+let obrasAcom = document.getElementById("obrasAcom");
+let obrasFin = document.getElementById("obrasFin");
+let btnGraficos = document.getElementById("btn-grafico");
+let conteudoObra = document.getElementById("conteudo");
+
 function obrasAcompanhadas (){
 
     var tabela = new google.visualization.DataTable();
@@ -12,14 +17,12 @@ function obrasAcompanhadas (){
 
     ]);
 
-    var grafico = new google.visualization.PieChart(document.getElementById('obrasAcom'));
+    var grafico = new google.visualization.PieChart(obrasAcom);
     grafico.draw(tabela);
 
 
     
 }
-
-google.charts.setOnLoadCallback(obrasAcompanhadas);
 
 
 function obrasFinalizadas (){
@@ -34,21 +37,27 @@ function obrasFinalizadas (){
 
     ]);
 
-    var grafico = new google.visualization.BarChart(document.getElementById('obrasFin'));
+    var grafico = new google.visualization.BarChart(obrasFin);
     grafico.draw(tabela);
 }
 
-google.charts.setOnLoadCallback(obrasFinalizadas);
 
 
 function mostrarGraficos() {
-    document.getElementById("graficos").style.display = "inline-block";
-    document.getElementById("graficos").style.width = "500px";
+    btnGraficos.style.color = "#A5CF3A";
+    btnGraficos.style.textDecoration = "underline";
+    obrasAcom.style.display = "block";
+    obrasFin.style.display = "block";
+    google.charts.setOnLoadCallback(obrasAcompanhadas);
+    google.charts.setOnLoadCallback(obrasFinalizadas);
   }
   
-
+  function ocultarGraficos() {
+    btnGraficos.style.color = "#000000";
+    btnGraficos.style.textDecoration = "none";
+    obrasAcom.style.display = "none";
+    obrasFin.style.display = "none";
+  }
   
-function mostrarConfigObra() {
-    document.getElementById("contaienr-form_ajusteObra").style.zIndex = "2";
-    
-}
+btnGraficos.addEventListener("click",mostrarGraficos);
+conteudoObra.addEventListener("click",ocultarGraficos);
