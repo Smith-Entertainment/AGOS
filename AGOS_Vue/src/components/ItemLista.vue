5a7c493e7f3b1c53efd3af04b678b16cebd2928e
Andre Mai Velasco <andremaivelasco@hotmail.com>
2023-04-16 16:52:42 -0300

    lista aparecendo (sem css)

---
+++ AGOS_Vue/src/components/ItemLista.vue
@@ -0,0 +1,26 @@
+<script>
+    export default{
+        props: {
+            obra: {}
+        }
+    }
+</script>
+
+<template>
+    <div class="container">
+        <img src={{obra.img}} class="obra_img" alt="Foto Obra">
+
+        <h2>Nome: {{obra.nome}}</h2>
+        <h3>Objeto: {{obra.objeto}}</h3>
+        <h3>Endereço: {{obra.logradouro}} {{obra.numero}}</h3>
+        <h3>Nº do Contrato: {{obra.contrato}}</h3>
+
+        <h3>Data de Inicio: {{obra.data_inicio}}</h3>
+        <h3>Data de Termino Previsto: {{obra.data_terminio}}</h3>
+        <h3>Valor do Contrato: R${{obra.valor_contrato}}</h3>
+    </div>
+</template>
+
+<style>
+
+</style>
