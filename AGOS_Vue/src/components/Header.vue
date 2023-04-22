575d396f8d7050cfec8d346b69ed177f3b051295
Cleyton da Silva Pires <cleytonxsilva@outlook.com>
2023-04-21 22:33:19 -0300

    classes adicionadas a navbar

--- AGOS_Vue/src/components/Header.vue
+++ AGOS_Vue/src/components/Header.vue
@@ -1,26 +1,43 @@
 
 <script>
-    console.log('Hello World!')
+export default {
+    name: 'Header'
+}
 </script>
 <template>
-    <div class="">
-        <picture class="">
-            <a href="Obras"><img src="../assets/img/logo.png" alt="Logo Observatorio" class=""></a>
-        </picture>
-
-        <button ><span class="material-symbols-outlined">menu</span></button> <!--botao menu mobile-->
-        <nav>
-            <a href="Obras">Obras</a> <!--provisorio ate ter as rotas-->
-            <a href="Usuarios">Usuários</a> <!---->
-            <a href="Relatorios">Relatorios</a> <!--provisorio ate ter as rotas-->
-        </nav>
-        <div class="">
-            <h4>Usuário</h4>
-
-            <a href="./html/login.html" class="">Sair</a>
-            <a href="#"><img class="" src="../assets/img/loginico.png" alt="Imagem usuário"></a>
+    <header class="row">
+        <div class="container" id="nav-container">
+            <div class="col-5">
+                <a href="Obras"><img src="../assets/img/logo.png" alt="Logo Observatorio" class="navbar-brand" id="logo"></a>
+            </div>
+            <div class="col-md-8">
+                <nav class="navbar navbar-expand-md"> <!--fixed-top?-->
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-links"
+                        aria-controls="navbar-links" aria-extend="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span></button> <!--botao menu mobile-->
+                    <div class="collapse navbar-collapse justify-content-center" id="navbar-links">
+                        <div class="navbar-nav">
+                            <a href="nav-item nav-link" id="obras-menu">Obras</a>
+                            <a href="nav-item nav-link" id="usuario-menu">Usuários</a>
+                            <a href="nav-item nav-link" id="relatorio-menu">Relatorios</a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+            <div class="col-5">
+                <h4>Usuário</h4>
+                <a href="#" class="">Sair</a>
+                <a href="#"><img src="../assets/img/loginico.png" alt="Imagem usuário" width="100"></a>
+            </div>
         </div>
-    </div>
+    </header>
 </template>
+<style>
+header {
+    background-color: rgb(0, 255, 225);
+}
 
-<style></style>
+.col-md-8 {
+    border: 2px solid black;
+}
+</style>
