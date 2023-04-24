90df72e4d89159f683119bd3931111ce5fa5b4b6
andre <andremaivelasco@hotmail.com>
2023-04-24 18:39:53 -0300

    titulo item obra e linha separada

--- AGOS_Vue/src/components/ItemLista.vue
+++ AGOS_Vue/src/components/ItemLista.vue
@@ -8,21 +8,27 @@
 
 <template>
     <div class="container">
-        <div class="row">
-            <img :src="obra.img" class="col-2" alt="Foto Obra">
+        <div class="row align-items-center justify-content-between">
+            <img :src="obra.img" class="col-md-3 col-sm-6" alt="Foto Obra">
 
             <div class="col">
-                <h2>Nome: {{obra.nome}}</h2>
-                <p>Objeto: {{obra.objeto}}</p>
-                <p>Endereço: {{obra.logradouro}} {{obra.numero}}</p>
-                <p>Nº do Contrato: {{obra.contrato}}</p>
-            </div>
+                <div class="row">
+                    <h2>Nome: {{obra.nome}}</h2>
+                </div>
+                
+                <div class="row">
+                    <div class="col">
+                        <p>Objeto: {{obra.objeto}}</p>
+                        <p>Endereço: {{obra.logradouro}} {{obra.numero}}</p>
+                        <p>Nº do Contrato: {{obra.contrato}}</p>
+                    </div>
 
-            <div class="col">
-                <h2><br></h2>
-                <p>Data de Inicio: {{obra.data_inicio}}</p>
-                <p>Data de Termino Previsto: {{obra.data_terminio}}</p>
-                <p>Valor do Contrato: R${{obra.valor_contrato}}</p>
+                    <div class="col">
+                        <p>Data de Inicio: {{obra.data_inicio}}</p>
+                        <p>Data de Termino Previsto: {{obra.data_terminio}}</p>
+                        <p>Valor do Contrato: R${{obra.valor_contrato}}</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
