897b5f0a5f95f07973394b7f7f31689d02e0fca6
Andre Mai Velasco <andremaivelasco@hotmail.com>
2023-06-01 02:49:30 -0300

    arrumei o redirecionamento da rota raiz

--- AGOS_Vue/src/router.js
+++ AGOS_Vue/src/router.js
@@ -3,7 +3,7 @@ import PerfilObra from './PerfilObra.vue';
 import ListaObra from './ListaObra.vue';
 import CadastroObra from './components/CadastroObra.vue';
 const routes = [
-  { path: '/', name: 'home', redirect: {name: ListaObra}},
+  { path: '/', name: 'home', redirect: {name: "ListaObra"}},
   { path: '/obra/:id', name: 'PerfilObra', component: PerfilObra},
   { path: '/lista-obras', name: 'ListaObra', component: ListaObra},
   { path: '/cadastro-obra', name: 'CadastroObra', component: CadastroObra}
