f179d7828315daccf083928049b3ae79e249dd2e
Luis <luiserr1394@gmail.com>
2023-07-15 15:20:58 -0300

    Client Envio pronto

--- AGOS/src/client/envioClient.ts
+++ AGOS/src/client/envioClient.ts
@@ -27,20 +27,45 @@ public async findAllEnvios():Promise<envioModel[]>{
   }
 
 
+  public async  cadastrarEnvio(envio:envioModel): Promise<void> {
+    
+    try{
+      return (await this.axiosClient.post(`/`,envioModel)).data;
+    }
+
+    catch(error:any){
+      return Promise.reject(error);
+    }
+
+  }
 
 
+  public async editarEnvio(id:number, envio:envioModel): Promise<void>{
+    try{
+      return (await this.axiosClient.put(`/${id}`,envioModel)).data
+    }
+
+    catch(error:any){
+
+      return Promise.reject(error.response)
+    }
+  }
 
 
-  
+  public async excluirEnvio(id:number):Promise<void>{
+
+    try{
+      return (await this.axiosClient.delete(`/${id}`) ).data
+    }
+
+    catch(error:any){
+      return Promise.reject(error.response)
+    }
+  }
 
 }
 
 
+export default new envioClient();
 
 
-
-
-
-
-
-}
