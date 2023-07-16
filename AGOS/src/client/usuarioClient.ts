5fc8d241d2340253b1e01022ca5bfdb0bc7ab9bf
andre <andremaivelasco@hotmail.com>
2023-07-16 11:59:41 -0300

    client usuario

---
+++ AGOS/src/client/usuarioClient.ts
@@ -0,0 +1,59 @@
+import axios, { AxiosInstance } from "axios";
+import { Usuario } from "@/model/usuarioModel";
+
+export class UsuarioClient{
+    private axiosClient : AxiosInstance;
+
+    constructor(){
+        this.axiosClient = axios.create({
+            baseURL: "http://localhost:8080/api",
+            headers: {"Content-type" : "application/json"}
+        });
+    }
+
+    public async findById(id : number) : Promise<Usuario> {
+        try{
+            return (await this.axiosClient.get<Usuario>(`/usuario?id=${id}`)).data;
+        }
+        catch(error : any){
+            return Promise.reject(error.response);
+        }
+    }
+
+    public async findAll() : Promise<Usuario[]> {
+        try{
+            return (await this.axiosClient.get<Usuario[]>(`/usuario/lista`)).data;
+        }
+        catch(error : any){
+            return Promise.reject(error.response);
+        }
+    }
+
+    public async cadastrar(usuario : Usuario) : Promise<string> {
+        try{
+            return (await this.axiosClient.post<string>(`/usuario`, usuario)).data;
+        }
+        catch(error : any){
+            return Promise.reject(error.response);
+        }
+    }
+
+    public async editar(id : number, usuario : Usuario) : Promise<string> {
+        try{
+            return (await this.axiosClient.put<string>(`/usuario?id=${id}`, usuario)).data;
+        }
+        catch(error : any){
+            return Promise.reject(error.response);
+        }
+    }
+
+    public async excluir(id : number) : Promise<string> {
+        try{
+            return (await this.axiosClient.delete<string>(`/usuario?id=${id}`)).data;
+        }
+        catch(error : any){
+            return Promise.reject(error.response);
+        }
+    }
+}
+export default new UsuarioClient();
