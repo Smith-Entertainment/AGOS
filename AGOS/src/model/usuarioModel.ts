d9b4e31e76ea4b676a18021855db85e87ffe6d70
andre <andremaivelasco@hotmail.com>
2023-07-16 11:43:12 -0300

    model usuario criado

---
+++ AGOS/src/model/usuarioModel.ts
@@ -0,0 +1,27 @@
+import { Obra } from "@/model/obraModel.ts"
+
+export class Usuario{
+    id !: number;
+
+    nome !: string;
+
+    usuario !: string;
+
+    email !: string;
+
+    cpf !: string;
+
+    celular !: string;
+
+    tituloEleitor !: string;
+
+    senha !: string;
+
+    nomePai : string;
+
+    nomeMae !: string;
+
+    dataNascimento !: Date;
+
+    obras : Array<Obra>
+}
