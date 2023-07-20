e94d7967ebe02e97d74397454d78bce0f50547df
Cleyton da Silva Pires <cleytonxsilva@outlook.com>
2023-07-20 00:53:23 -0300

    header sem estilizar e add img

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,24 +8,34 @@ export default {
     <header class="row">
         <div class="container" id="nav-container">
             <div class="col-2">
-                <a href=""><img src="@/assets/logo.png" alt="Logo Observatorio" class="navbar-brand" id="logo"></a>
+                <a href=""><img src="@/assets/img/logo.png" alt="Logo Observatorio" class="navbar-brand" id="logo"></a>
             </div>
-            <div class="col-3">
+            <div class="col-8">
                 <nav class="navbar navbar-expand-md"> <!--fixed-top?-->
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-links"
                         aria-controls="navbar-links" aria-extend="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span></button> <!--botao menu mobile-->
                     <div class="collapse navbar-collapse justify-content-center" id="navbar-links">
                         <div class="navbar-nav">
-                            <a href="" class="nav-item nav-link" id="obras-menu">Obras</a>
-                            <a class="nav-item nav-link" id="usuario-menu">Usuários</a>
-                            <a class="nav-item nav-link" id="relatorio-menu">Relatorios</a>
+                            <a href="" class="nav-item nav-link" id="obras-menu">Obras</a><!--TROCAR POR ROUTERLINK-->
+                            <a class="nav-item nav-link" id="usuario-menu">Usuários</a><!--TROCAR POR ROUTERLINK-->
+                            <a class="nav-item nav-link" id="relatorio-menu">Relatorios</a><!--TROCAR POR ROUTERLINK-->
                         </div>
                     </div>
                 </nav>
             </div>
-            <div class="col-1">
-         
+            <div class="col-2">
+                <div class="row">
+                <div>
+                    <a href="" class="col-4">Usuário</a>
+                </div>
+                <div>
+                    <a href="" class="col-4">Sair</a>
+                </div>
+                <div>
+                    <img src="@/assets/img/iconeperfillogin.png" alt="icone-perfil-usuario" width="50">
+                </div>
+                </div>
             </div>
         </div>
     </header>
