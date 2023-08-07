bbc4ecb8fc1be9e8bc9d6352b7ba0bab744e4d85
andre <andremaivelasco@hotmail.com>
2023-08-07 12:25:55 -0300

    rotas antigas adicionadas no router[

--- src/router/index.ts
+++ src/router/index.ts
@@ -16,7 +16,32 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('@/views/Vue/AboutView.vue')
-    }
+    },
+    /* { 
+      path: '/', 
+      name: 'home', 
+      redirect: {name: "ListaObra"}
+    },
+    { 
+      path: '/obra/:id', 
+      name: 'PerfilObra', 
+      component: PerfilObra
+    },
+    { 
+      path: '/lista-obras', 
+      name: 'ListaObra', 
+      component: ListaObra
+    },
+    { 
+      path: '/cadastro-obra', 
+      name: 'CadastroObra', 
+      component: CadastroObra
+    },
+    { 
+      path: '/cadastro-usuario', 
+      name: 'CadastroUsuario', 
+      component: CadastroUsuario
+    } */
   ]
 })
 
